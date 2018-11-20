d7bd7badef951833beff20e87d76876ad7a106c1
lansehuiyi6 <zhan.li@wanglutech.com>
2018-11-20 10:56:44 +0800

    update print comments

--- initConfig.js
+++ initConfig.js
@@ -19,7 +19,7 @@ async function init() {
       }
       console.log(crossTokens);
     } catch (err) {
-      console.log(err);
+      console.log("Storeman agent init error, plz check the paras and try again.", err);
       process.exit();
     }
   } else {
