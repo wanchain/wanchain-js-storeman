b50f4308aa3fd801cf443cb2e8773f65a4761a0c
wl <zhwir@163.com>
2020-07-27 18:23:07 +0800

    fix incentive bug

--- agent/osm/src/openStoreman.js
+++ agent/osm/src/openStoreman.js
@@ -1,4 +1,7 @@
 const wanchain = require('./utils/wanchain');
+const Logger = require('../../../comm/logger.js');
+
+const logger = new Logger("openStoreman-" + global.argv.c, "log/openStoreman.log", "log/openStoreman_error.log", global.argv.loglevel);
 
 const GroupStatus = {
   none:0, 
@@ -10,6 +13,9 @@ const GroupStatus = {
   unregistered:6, 
   dismissed:7,
 }
+
+const INVALID_GROUP_ID = '0x0000000000000000000000000000000000000000000000000000000000000000';
+
 const wkAddr = wanchain.selfAddress;
 
 let lastIncentivedDay = 0;
@@ -22,8 +28,6 @@ async function sleep(time) {
 	});
 };
 
-
-
 async function handlerOpenStoremanIncentive(wkaddr){
   let curDay = Date.now()/1000/60/60/24;
   if(curDay == lastIncentivedDay){
@@ -31,21 +35,24 @@ async function handlerOpenStoremanIncentive(wkaddr){
   }
   while(1){
     let txhash = await wanchain.sendToIncentive(wkaddr)
-    global.logger.info(arguments.callee.name+" txhash:", txhash);
-    let receipt = await wanchain.sendToIncentive(txhash, arguments.callee.name)
-    if(!receipt  ||  !receipt.logs || receipt.logs.topics[3]==1){
+    logger.info(arguments.callee.name+" txhash:", txhash);
+    let receipt = await wanchain.getTxReceipt(txhash, arguments.callee.name)
+    if(!receipt || !receipt.logs || receipt.logs.topics[3]==1){
       lastIncentivedDay = curDay;
       break;
     }
   }
 }
+
 async function handlerOpenStoremanStatus(group){
-  let cur = Date.now()/1000;
-  switch(group.status){
+  let cur = parseInt(Date.now()/1000);
+  switch(parseInt(group.status)){
     case GroupStatus.curveSeted: // toselect.
       if(wkAddr == group.selectedNode[0]){ // is Leader
-        if(cur > group.registerTime+group.registerDuration){
-          await wanchain.sendToSelect(group.groupId);
+        let selectTime = parseInt(group.registerTime) + parseInt(group.registerDuration);
+        if(cur > selectTime){
+          let txHash = await wanchain.sendToSelect(group.groupId);
+          logger.info("osm select txHash: %s", txHash);
         }
       }
       break;
@@ -71,25 +78,26 @@ async function handlerOpenStoremanStatus(group){
       break;
   }
 }
-async function handlerOpenStoreman(logger) { 
+
+async function handlerOpenStoreman() {
+  await wanchain.updateNounce();
   while (1) {
     logger.info("********************************** handlerOpenStoreman start **********************************");
-
-    try{
-      // get my group
+    try {
       let sk = await wanchain.getSkbyAddr(wkAddr);
-      let group = await wanchain.getGroupById(sk.groupId);
-      handlerOpenStoremanIncentive(wkAddr);
-
-      handlerOpenStoremanStatus(group);
-      if(sk.nextGrupId){
-        let groupNext = await wanchain.getGroupById(sk.nextGrupId);
+      if (sk.groupId) {
+        let group = await wanchain.getGroupById(sk.groupId);
+        handlerOpenStoremanIncentive(wkAddr);
+        handlerOpenStoremanStatus(group);
+      }
+      if (sk.nextGroupId != INVALID_GROUP_ID) {
+        let groupNext = await wanchain.getGroupById(sk.nextGroupId);
         handlerOpenStoremanStatus(groupNext);
       }
-    }catch(err){
+    } catch(err) {
       logger.error("handlerOpenStoreman error:", err);
     }
-    await sleep(1000*60*10);
+    await sleep(1000*60);
   }
 }
 
